94031ad9793af06029705315433458c522a7930a
daveiainadams <92177279+daveiainadams@users.noreply.github.com>
2025-06-05 15:31:52 +0100

    Fix Google Fonts imports

--- app/src/main/java/com/dejvik/stretchhero/ui/theme/Type.kt
+++ app/src/main/java/com/dejvik/stretchhero/ui/theme/Type.kt
@@ -2,14 +2,13 @@ package com.dejvik.stretchhero.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.googlefonts.Font
-import androidx.compose.ui.text.googlefonts.FontFamily
-import androidx.compose.ui.text.googlefonts.FontProvider
 import androidx.compose.ui.text.googlefonts.GoogleFont
 import com.dejvik.stretchhero.R
 
-private val provider = FontProvider(
+private val provider = GoogleFont.Provider(
     providerAuthority = "com.google.android.gms.fonts",
     providerPackage = "com.google.android.gms",
     certificates = R.array.com_google_android_gms_fonts_certs
